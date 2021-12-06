b7a628036e25a5a39ca403a9c4c066186a5e149a
Carmelo Fallone <carmelo.ve@gmail.com>
2021-12-06 17:13:10 -0400

    Featured / create mailable and command for send emails

---
+++ app/Console/Commands/emailmassive.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Mail\massivemails;
+use App\Models\Email;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Mail;
+
+class emailmassive extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'command:name';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $emails = Email::where('status', 'nosend')->get();
+
+        foreach ($emails as $val) {
+            try {
+                Mail::to($val->to)->send(new massivemails($val->subject, $val->content));
+                $val->status = 'send';
+                $val->save();
+            } catch (\Throwable $th) {
+                //throw $th;
+                Log('Has error ocurred in: ' . $th->getMessage() . 'líne' . $th->getLine());
+            }
+        }
+    }
+}
---
+++ resources/views/emails/send.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Mensaje nuevo</title>
+</head>
+<body>
+    <p>Nuevo mensaje, asunto: {{ $subject }}</p>
+
+    <p>{{ $content }}</p>
+</body>
+</html>
